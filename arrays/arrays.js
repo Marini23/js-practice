c4f4d2f7d85d5bc6976ef7343773fbc1085b1f3d
Marini23 <marina.smile1m@gmail.com>
2025-11-14 23:56:57 +0100

    02

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -32,6 +32,33 @@ const twoSumAlternative = function (nums, target) {
   }
 };
 
-console.log(twoSum([2, 7, 11, 15], 9));
+// console.log(twoSum([2, 7, 11, 15], 9));
 
-console.log(twoSumAlternative([2, 7, 11, 15], 9));
+// console.log(twoSumAlternative([2, 7, 11, 15], 9));
+
+// Task 2
+
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+// Consider the number of unique elements in nums to be k​​​​​​​​​​​​​​. After removing duplicates, return the number of unique elements k.
+
+// The first k elements of nums should contain the unique numbers in sorted order. The remaining elements beyond index k - 1 can be ignored.
+
+const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+
+const removeDuplicates = function (nums) {
+  if (nums.length === 0) return 0;
+
+  let k = 1; // pointer for the position of the next unique element
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      nums[k] = nums[i]; // place unique element
+      k++;
+    }
+  }
+
+  return k;
+};
+
+console.log(removeDuplicates(nums));
